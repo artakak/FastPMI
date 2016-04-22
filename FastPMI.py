1f0116a546cdcf455e8123bf9173fdd77f988fe4
win7_test <a260641139@ya.ru>
2016-04-22 18:49:42 +0400

    cbgfgf

--- FastPMI.py
+++ FastPMI.py
@@ -1,40 +1,56 @@
 # -*- coding: utf-8 -*-
 import requests, json
+import random
 from faker import Factory
-fake = Factory.create('ru_RU')
+import threading
 
+fake = Factory.create('ru_RU')
+fake_US = Factory.create()
+count = 0
+s = requests.Session()
 def regPMI():
-    url = "https://pmi.experium.ru/SupportSrv/SupportSrv.svc/Support/v1/pmi/registration"
-    data = (fake.name(),fake.name(),'blablabla@dfgfdgdf.com','test123')
-    for t in data:
-        print t
+    global count
+    global s
+    url = "http://webtest.experium.ru:90/SupportSrv/SupportSrv.svc/Support/v1/pmi/registration"
+    data = (fake.last_name_female(),fake.first_name_female(), fake_US.safe_email(), fake.password(length=10, special_chars=False, digits=True, upper_case=True, lower_case=True), fake.first_name_male()+u'овна')
+    #for t in data:
+        #print t
     regdata = {'lastName':data[0],'firstName':data[1],'login':data[2],'confirmPassword':data[3],'password':data[3]}
-    regreq = requests.post(url, json=regdata)
+    regreq = s.post(url, json=regdata)
     print (regreq.status_code)
-    print (regreq.text)
+    #print (regreq.text)
     response = json.loads(regreq.text)
 
-    url2 = "https://pmi.experium.ru/SupportSrv/SupportSrv.svc/Support/v1/pmi/ra/submission"
+    url2 = "http://webtest.experium.ru:90/SupportSrv/SupportSrv.svc/Support/v1/pmi/gr/submission"
     headers = {'Authorization':'Token %s'%response['token']}
     postdata1 = {"draft":True}
 
-    postreq = requests.post(url2, headers=headers, json=postdata1)
+    postreq = s.post(url2, headers=headers, json=postdata1)
     print (postreq.status_code)
-    print (postreq.text)
+    #print (postreq.text)
     response1 = json.loads(postreq.text)
     postdata = {"token": response['token'], "attachedDocument": True, "draft": False, "lastChangeDate": "",
-                "personal": {"firstName": data[1], "middleName": "", "lastName": data[0],
+                "personal": {"firstName": data[1], "middleName": data[4], "lastName": data[0],
                              "birthDate": ""},
                 "contacts": {"phone": "74565464565", "mobile": "74565465464", "email": data[3],
                              "mobileCode": "", "phoneCode": ""},
                 "addresses": [{"countryCode": 1, "townkey": 19, "region": 4}], "education": [
             {"levelCode": 4, "institute": 3, "otherInstituteName": "", "speciality": "специальность",
              "startYearGraduation": 1988, "yearGraduation": 1999}],
-                "graduateInfo": {"englishKnowLevel": 32, "businessArea": 15},
-                "sourceInfo": {"sourceId": 4, "sourceOther": ""}, "id": response1['id']}
+                "graduateInfo": {"englishKnowLevel": 32, "businessArea": 12},
+                "sourceInfo": {"sourceId": 4, "sourceOther": ""}, "id": response1['id']+10}
 
-    postreq = requests.post(url2, headers=headers, json=postdata)
+    postreq = s.post(url2, headers=headers, json=postdata)
     print (postreq.status_code)
-    print (postreq.text)
+    count+=1
+    print ('***%d***'%count)
+    #print (postreq.text)
 
-regPMI()
+def tread():
+    for _ in range(10000):
+        try:
+            regPMI()
+        except: pass
+
+for _ in range(4):
+    threading.Thread(target=tread).start()
